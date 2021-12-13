0e601dea42ed97249dbe7db003c3df0158b494bc
YUHEI <76258455+yuehi@users.noreply.github.com>
2021-12-13 23:01:00 +0900

    home

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1です</h1>
+      <Link to="/page1/DetailA">DetailA</Link>
+      <br />
+      <Link to="/page1/DetailB">DetailB</Link>
     </div>
   );
 };
