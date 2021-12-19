fdcd465a56eef0b9d6e99c7ae9b0a63ebafef333
YUHEI <76258455+yuehi@users.noreply.github.com>
2021-12-19 21:58:44 +0900

    home

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,36 +1,34 @@
-import { Switch, Route } from "react-router-dom";
+import { Swicth, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
-
+import { page1Routes } from "./Page1Routes";
 
 export const Router = () => {
-  return(
-    <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route 
-        path="/page1" 
-        render={({match: { url } })=> (
+  return (
+    <Swicth>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
           <Swicth>
-            <Route exact path={url}>
-          <page1 />
-            <Route path={'${url}/detailA'}>
-              <Page1DetailA />
-                </Route>
-            <Route path={'${url}/detailB'}>
-              <Page1DetailB />
-                </Route>
+            {page1Routes.map((route) => {
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>;
+            })}
           </Swicth>
-        )} 
-        />
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
-  )
-} 
+        )}
+      />
+
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </Swicth>
+  );
+};
